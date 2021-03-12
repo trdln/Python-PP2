6cb159391a0e86ad3d2313a1aec4f052fe7801a9
Nura-21 <n_turdalin@kbtu.kz>
2021-03-13 01:10:15 +0600

    fixes, adding sphere for head, writing moving code in Class

--- Pygame/Projects/MySnakeV1/snakev2.py
+++ Pygame/Projects/MySnakeV1/snakev2.py
@@ -10,6 +10,8 @@ win = pygame.display.set_mode((RES,RES+80))
 pygame.display.set_caption('Snake')
 back = pygame.image.load('tex/back.jpg')
 font = pygame.font.Font('freesansbold.ttf', 32)
+dir = {'W':True , 'S':True , 'A':True , 'D':True }
+
 
 class Snake:
     def __init__(self):
@@ -32,12 +34,24 @@ class Snake:
         self.run = True
 
     def draw(self):
-        [(pygame.draw.rect(win, pygame.Color('green'), (i,j,SIZE - 1 ,SIZE - 1))) for i,j in self.snake]
+        if self.dx == 0 and self.dy == 0:
+            (pygame.draw.circle(win, pygame.Color('green'), (self.x+SIZE / 2,self.y+SIZE / 2),12)) in self.snake[0]
+        if self.dx == 1 and self.dy == 0:
+            (pygame.draw.circle(win, pygame.Color('green'), (self.x  + SIZE  - SIZE / 20 ,self.y+SIZE / 2),12)) in self.snake[0]
+        if self.dx == -1 and self.dy == 0:
+            (pygame.draw.circle(win, pygame.Color('green'), (self.x-SIZE / 20,self.y+SIZE / 2),12)) in self.snake[0]
+        if self.dx == 0 and self.dy == 1:
+            (pygame.draw.circle(win, pygame.Color('green'), (self.x+SIZE / 2 ,self.y+SIZE),12)) in self.snake[0]
+        if self.dx == 0 and self.dy == -1:
+            (pygame.draw.circle(win, pygame.Color('green'), (self.x+SIZE / 2,self.y- SIZE / 20 ),12)) in self.snake[0]
+        
+        [(pygame.draw.rect(win, pygame.Color('green'), (i,j,SIZE  - 1,SIZE - 1 ))) for i,j in self.snake[1:]]
         win.blit(self.image, (self.apple))
 
  
         
     def gameover(self):
+        global dir
         if len(self.snake) != len(set(self.snake)):
             self.x , self.y = randrange(0,RES,SIZE),randrange(0,RES,SIZE)
             self.apple = randrange(0,RES,SIZE),randrange(0,RES,SIZE)
@@ -49,8 +63,8 @@ class Snake:
             self.leng = 1
             self.speed = 10
             self.dx,self.dy = 0, 0
-            
-            
+            win.blit(back,(0,0))
+            dir = {'W':True , 'S':True , 'A':True , 'D':True }
             
         if self.x < 0 or self.x > RES - SIZE or self.y < 0 or self.y > RES-SIZE:
             self.x,self.y = randrange(0,RES,SIZE),randrange(0,RES,SIZE)
@@ -63,55 +77,61 @@ class Snake:
             self.leng = 1
             self.speed = 10
             self.dx,self.dy = 0, 0
+            win.blit(back,(0,0))
+            dir = {'W':True , 'S':True , 'A':True , 'D':True }
 
     def eat(self):
         if self.snake[-1] == self.apple:
             self.apple = randrange(0,RES,SIZE),randrange(0,RES,SIZE)
             self.leng +=1
-            self.speed +=0.5
+            self.speed +=0.25
 
+
+    def move(self,keys):
+        global dir
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and dir['A']:
+                self.dx,self.dy = -1,0
+                dir = {'W':True , 'S':True , 'A':True , 'D':False }
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and dir['D']:
+                self.dx,self.dy =  1,0
+                dir = {'W':True , 'S':True , 'A':False , 'D':True }
+        if (keys[pygame.K_UP] or keys[pygame.K_w]) and dir['W']:
+                self.dx,self.dy = 0,-1
+                dir = {'W':True , 'S':False , 'A':True , 'D':True }
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and dir['S']:
+                self.dx,self.dy = 0, 1
+                dir = {'W':False , 'S':True , 'A':True , 'D':True }
+
+        
         
 snake = Snake()
 clock = pygame.time.Clock()
 win.blit(back, (0, 0))
-dirs = {'W':True , 'S':True , 'A':True , 'D':True }
+
+
 while snake.run:
     win.blit(back,(0,0))
-
+    
     text = font.render(f'Score : {snake.leng}                             Best : {snake.best}', True, pygame.Color('white'))
     textRect = text.get_rect()
     textRect.center = (int(RES / 2 ), RES + 35)
     win.blit(text, textRect)
     clock.tick(snake.speed)
+        
     snake.draw()
-    snake.draw
-    
+        
     pygame.display.flip()
     snake.eat()
     if snake.gameover():
-        dirs = {'W':True , 'S':True , 'A':True , 'D':True }
         snake.leng = 1
 
     snake.x+=snake.dx * SIZE
     snake.y+=snake.dy * SIZE
     snake.snake.append((snake.x,snake.y))
     snake.snake = snake.snake[-snake.leng:]
-
+    
     keys = pygame.key.get_pressed()
-
-    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and dirs['A']:
-        snake.dx,snake.dy = -1,0
-        dirs = {'W':True , 'S':True , 'A':True , 'D':False }
-    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and dirs['D']:
-        snake.dx,snake.dy =  1,0
-        dirs = {'W':True , 'S':True , 'A':False , 'D':True }
-    if (keys[pygame.K_UP] or keys[pygame.K_w]) and dirs['W']:
-        snake.dx,snake.dy = 0,-1
-        dirs = {'W':True , 'S':False , 'A':True , 'D':True }
-    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and dirs['S']:
-        snake.dx,snake.dy = 0, 1
-        dirs = {'W':False , 'S':True , 'A':True , 'D':True }
-
+    snake.move(keys)
     
 
     for event in pygame.event.get():
@@ -119,6 +139,5 @@ while snake.run:
             snake.run = False
     pygame.display.update()
 
-pygame.quit()
 
-    
+        
